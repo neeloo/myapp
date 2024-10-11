0b16c54cdcb3d2029d6813d7c0a3298bfcf21037
neeloo patel <nilusingh945164@gmai.com>
2024-10-11 16:42:16 +0530

    npm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AppA from './component/AllItem';
 
 function App() {
   return (
    <> 
-    {/* <AppA/> */}
     <AppA/>
       
    </>
